cc3c68ba851f642f359141be1286ddfab6fba6cf
Sagarmogli8976 <114072291+Sagarmogli8976@users.noreply.github.com>
2026-04-06 02:55:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const stopBtn = document.querySelector(".stop");
+
+let currentAudio = null;
+
+// Play sound
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const soundName = button.getAttribute("data-sound");
+
+        // Stop previous audio if playing
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+
+        currentAudio = new Audio(`sounds/${soundName}.mp3`);
+        currentAudio.play();
+    });
+});
+
+// Stop sound
+stopBtn.addEventListener("click", () => {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+});
